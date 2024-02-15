4db72fd0d44584fdff29e5cc225f7f99111db6be
bigjeff96 <joepep271996@gmail.com>
2024-02-15 17:43:36 +0100

    feat: add functions for manipulating rectangles
    This commit adds several functions for working with rectangles. The functions added include:
    - `rectangle_make` to create a rectangle with given coordinates
    - `rectangle_valid` to check if a rectangle is valid
    - `rectangle_intersection` to find the intersection of two rectangles
    - `rectangle_bounding` to find the bounding rectangle of two rectangles
    - `rectangle_equals` to check if two rectangles are equal
    - `rectangle_contains` to check if a point is within a rectangle
    
    These functions are useful for performing operations on rectangles in a program.

---
+++ gui.h
@@ -0,0 +1,18 @@
+struct Rectangle {
+    int l, r, t, b;
+};
+
+struct Global_state {
+};
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) > (b) ? (b) : (a))
+
+Rectangle rectangle_make(int l, int r, int t, int b);
+bool rectangle_valid(Rectangle r);
+Rectangle rectangle_intersection(Rectangle a, Rectangle b); // can be invalid
+Rectangle rectangle_bounding(Rectangle a, Rectangle b);
+bool rectangle_equals(Rectangle a, Rectangle b);
+bool rectangle_contains(Rectangle a, int x, int y);
+
+
